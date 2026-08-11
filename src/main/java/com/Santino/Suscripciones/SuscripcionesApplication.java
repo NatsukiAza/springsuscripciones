d7a8c9f5fe0be8fc29cbe6fd2c0166034c702581
NatsukiAza <70611342+NatsukiAza@users.noreply.github.com>
2026-08-11 00:12:00 -0300

    Estructura base
    Estructura base del proyecto, lista para implementar controllers, repository, services

---
+++ src/main/java/com/Santino/Suscripciones/SuscripcionesApplication.java
@@ -0,0 +1,13 @@
+package com.Santino.Suscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuscripcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuscripcionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Santino/Suscripciones/SuscripcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Santino.Suscripciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuscripcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
